b069614135500504d5b98facf13ca1a7a82151be
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-08-12 12:33:51 +0300

    Task 01 - create markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// Task 1
+const galleryEl = document.querySelector(".gallery");
+const imgElements = galleryItems
+  .map(({ original, preview, description }) => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src= "${preview}" data-source="${original}" alt= "${description}"/>
+    </a>
+    </div>`
+  ).join('');
 
+galleryEl.insertAdjacentHTML("beforeend", imgElements);
+// console.log(galleryEl);
 console.log(galleryItems);
